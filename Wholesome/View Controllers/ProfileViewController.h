6a2aa7c52e621f5ae71116ea0ce579663a6268c1
athomas9195 <anna.tina.thomas@gmail.com>
2021-07-13 16:26:39 -0700

    setup

---
+++ Wholesome/View Controllers/ProfileViewController.h
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.h
+//  Wholesome
+//
+//  Created by Anna Thomas on 7/13/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ProfileViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Wholesome/View Controllers/ProfileViewController.m
@@ -0,0 +1,31 @@
+//
+//  ProfileViewController.m
+//  Wholesome
+//
+//  Created by Anna Thomas on 7/13/21.
+//
+
+#import "ProfileViewController.h"
+
+@interface ProfileViewController ()
+
+@end
+
+@implementation ProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
