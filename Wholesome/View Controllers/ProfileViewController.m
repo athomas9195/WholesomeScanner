1149cda8a0d94ef0cb5e259bc0c7d421b1a63b1e
athomas9195 <anna.tina.thomas@gmail.com>
2021-07-14 16:47:52 -0700

    scroll view

--- Wholesome/View Controllers/ProfileViewController.m
+++ Wholesome/View Controllers/ProfileViewController.m
@@ -8,13 +8,23 @@
 #import "ProfileViewController.h"
 
 @interface ProfileViewController ()
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIView *contentView;
 
 @end
 
 @implementation ProfileViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
+    
+  
+    [self.scrollView addSubview:self.contentView];//if the contentView is not already inside your scrollview in your xib/StoryBoard doc
+
+    self.scrollView.contentSize = self.contentView.frame.size; //sets ScrollView content size
+    
+    self.scrollView.delegate = self;
     // Do any additional setup after loading the view.
 }
 
